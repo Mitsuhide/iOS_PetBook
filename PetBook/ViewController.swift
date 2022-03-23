d09fc4f3640a012a73a5f0a06dba5f1c81a780f4
Tony Top <ttop@octo.com>
2022-03-23 10:10:32 +0100

    screen done

--- PetBook/ViewController.swift
+++ PetBook/ViewController.swift
@@ -9,9 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var containerView: UIView!
+    
+    @IBOutlet var friendsImageView: [UIImageView]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupContainerView()
+        setupFriendsImageView()
+    }
+    
+    func setupContainerView() {
+        containerView.layer.cornerRadius = 20
+        containerView.layer.shadowColor = UIColor.gray.cgColor
+        containerView.layer.shadowRadius = 4
+        containerView.layer.shadowOpacity = 0.75
+        containerView.layer.shadowOffset = CGSize(width: 0, height: -3)
+    }
+    
+    func setupFriendsImageView(){
+        friendsImageView.forEach{friend in
+            let friendFrame = friend.frame
+            let corner = friendFrame.height / 2
+            friend.layer.cornerRadius = corner
+            friend.layer.borderColor = UIColor.gray.cgColor
+            friend.layer.borderWidth = 2
+        }
     }
 
 
